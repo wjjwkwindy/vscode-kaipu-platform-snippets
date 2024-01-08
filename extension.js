f4adad20ab2e31045bd84c4fc6c367a174b547ba
wjjwkwindy <wjjwkwindy>
2024-01-08 14:33:09 +0800

    feat: 新增设置界面高亮颜色、样式选项

--- extension.js
+++ extension.js
@@ -1,12 +1,12 @@
 const vscode = require('vscode');
 
 // 默认高亮样式
-const kaipuHighlight = {
+let kaipuHighlight = {
 	name: "Kaipu Tag Hightlight",
 	scope: ["entity.name.tag.html.kaipu.tag"],
 	settings: {
-		foreground: "#53aee2",
-		fontStyle: "italic underline"
+		foreground: "",
+		fontStyle: ""
 	}
 };
 
@@ -17,22 +17,30 @@ function activate(context) {
 	// 监听设置更改事件
 	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
 		if (e.affectsConfiguration('kaipu.injectTagHighlightToSetting')) {
-			if (getCurrentHighlightStatus()) {
+			if (getConfig('kaipu.injectTagHighlightToSetting')) {
 				injectHighlight();
 			} else {
 				removeHighlight();
 			}
 		}
+
+		if(e.affectsConfiguration('kaipu.injectTagHighlightForeground') || e.affectsConfiguration('kaipu.injectTagHighlightFontStyle')){
+			updateHighlightStyle(getConfig('kaipu.injectTagHighlightForeground'), getConfig('kaipu.injectTagHighlightFontStyle'));
+		}
 	}));
 
 	// 注册命令
 	context.subscriptions.push(vscode.commands.registerCommand('inject-tag-highlight-to-setting.kaipu', () => {
-		
-		if (!getCurrentHighlightStatus()) {
-			return;
+		if(getConfig('kaipu.injectTagHighlightForeground')){
+			kaipuHighlight.settings.foreground = getConfig('kaipu.injectTagHighlightForeground');
 		}
-
-		injectHighlight();
+		if(getConfig('kaipu.injectTagHighlightFontStyle')){
+			kaipuHighlight.settings.fontStyle = getConfig('kaipu.injectTagHighlightFontStyle');
+		}
+		
+		if (getConfig('kaipu.injectTagHighlightToSetting')) {
+			injectHighlight();
+		}
 	}));
 
 	// 自动执行
@@ -91,10 +99,35 @@ function removeHighlight() {
 }
 
 /**
- * 获取当前配置状态
+ * 更新高亮样式
  */
-function getCurrentHighlightStatus(){
-	return vscode.workspace.getConfiguration().get('kaipu.injectTagHighlightToSetting');
+function updateHighlightStyle(foreground, fontStyle){
+	const globalConfig = vscode.workspace.getConfiguration();
+	const value = globalConfig.get('editor.tokenColorCustomizations');
+
+	if (value['textMateRules']?.length) {
+		value['textMateRules'] = value['textMateRules'].map(item => {
+			if (item.name === kaipuHighlight.name) {
+				item.settings.foreground = foreground;
+				item.settings.fontStyle = fontStyle;
+			}
+
+			return item;
+		});
+	}
+
+	globalConfig.update('editor.tokenColorCustomizations', value, vscode.ConfigurationTarget.Global);
+
+	console.log('更新 kaipu tag 高亮成功');
+}
+
+/**
+ * 获取当前配置状态
+ * @param {string} configName 配置名
+ * @returns {any} 配置值
+ */
+function getConfig(configName){
+	return vscode.workspace.getConfiguration().get(configName);
 }
 
 module.exports = {
