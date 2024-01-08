e0ff19e7f6e43bfb4509e301cee266d2bb354462
wjjwkwindy <wjjwkwindy>
2024-01-08 11:33:10 +0800

    feat: 新增设置界面是否开始高亮选项

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode');
 
-// 高亮样式
+// 默认高亮样式
 const kaipuHighlight = {
 	name: "Kaipu Tag Hightlight",
 	scope: ["entity.name.tag.html.kaipu.tag"],
@@ -14,23 +14,26 @@ const kaipuHighlight = {
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// 注册命令
-	let disposable = vscode.commands.registerCommand('inject-tag-highlight-to-setting.kaipu', function () {
-		const globalConfig = vscode.workspace.getConfiguration();
-		const value = globalConfig.get('editor.tokenColorCustomizations');
-
-		if (!value['textMateRules']) {
-			value['textMateRules'] = [];
-		} else {
-			value['textMateRules'] = value['textMateRules'].filter(item => item.name !== kaipuHighlight.name);
+	// 监听设置更改事件
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+		if (e.affectsConfiguration('kaipu.injectTagHighlightToSetting')) {
+			if (getCurrentHighlightStatus()) {
+				injectHighlight();
+			} else {
+				removeHighlight();
+			}
 		}
-		value['textMateRules'].push(kaipuHighlight);
+	}));
 
-		globalConfig.update('editor.tokenColorCustomizations', value, vscode.ConfigurationTarget.Global);
+	// 注册命令
+	context.subscriptions.push(vscode.commands.registerCommand('inject-tag-highlight-to-setting.kaipu', () => {
+		
+		if (!getCurrentHighlightStatus()) {
+			return;
+		}
 
-		console.log('注入 kaipu tag 高亮成功');
-	});
-	context.subscriptions.push(disposable);
+		injectHighlight();
+	}));
 
 	// 自动执行
 	vscode.commands.executeCommand('inject-tag-highlight-to-setting.kaipu');
@@ -50,6 +53,48 @@ function deactivate() {
 	globalConfig.update('editor.tokenColorCustomizations', value, vscode.ConfigurationTarget.Global);
 
 	console.log('卸载 kaipu tag 高亮成功');
+}
+
+/**
+ * 注入高亮样式
+ */
+function injectHighlight() {
+	const globalConfig = vscode.workspace.getConfiguration();
+	const value = globalConfig.get('editor.tokenColorCustomizations');
+
+	if (!value['textMateRules']) {
+		value['textMateRules'] = [];
+	} else {
+		value['textMateRules'] = value['textMateRules'].filter(item => item.name !== kaipuHighlight.name);
+	}
+	value['textMateRules'].push(kaipuHighlight);
+
+	globalConfig.update('editor.tokenColorCustomizations', value, vscode.ConfigurationTarget.Global);
+
+	console.log('注入 kaipu tag 高亮成功');
+}
+
+/**
+ * 清除高亮样式
+ */
+function removeHighlight() {
+	const globalConfig = vscode.workspace.getConfiguration();
+	const value = globalConfig.get('editor.tokenColorCustomizations');
+
+	if (value['textMateRules']?.length) {
+		value['textMateRules'] = value['textMateRules'].filter(item => item.name !== kaipuHighlight.name);
+	}
+
+	globalConfig.update('editor.tokenColorCustomizations', value, vscode.ConfigurationTarget.Global);
+
+	console.log('清除 kaipu tag 高亮成功');
+}
+
+/**
+ * 获取当前配置状态
+ */
+function getCurrentHighlightStatus(){
+	return vscode.workspace.getConfiguration().get('kaipu.injectTagHighlightToSetting');
 }
 
 module.exports = {
